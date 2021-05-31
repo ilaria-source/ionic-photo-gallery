17f4253c3a7bf5abe1c476db84fe7fe450df48e5
ilaria-source <ilaria.secchi@avanade.com>
2021-05-31 17:50:42 +0200

    tab2 vede la fotocamera e scatta foto

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+
+
+public async addNewToGallery()
+{
+  const capturedPhoto = await Camera.getPhoto({
+    resultType: CameraResultType.Uri,
+    source: CameraSource.Camera,
+    quality: 100
+  });
+}
+}
